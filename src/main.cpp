d87e12a8282e2abcd2a602f56dc002008291bf31
hunterd129 <hunterd129@icloud.com>
2026-09-21 16:37:53 -0600

    fixed icons. app-icon & notif icon render now

--- src/main.cpp
+++ src/main.cpp
@@ -56,16 +56,16 @@ int main() {
         return 1;
     }
 
-    auto [body, image_path] = get_update_status();
+    auto [body, status_icon] = get_update_status();
 
     NotifyNotification *notification = notify_notification_new(
-        "Upgrade Notify", body.c_str(), "document-save-symbolic");
+        "Upgrade Notify",
+        body.c_str(),
+        status_icon.c_str()
+        );
 
     notify_notification_set_urgency(notification, NOTIFY_URGENCY_CRITICAL);
-
-    GVariant *image_path_variant = g_variant_new_string(image_path.c_str());
-    notify_notification_set_hint(notification, "image-path", image_path_variant);
-
+    notify_notification_set_app_icon(notification, "document-save-symbolic");
     notify_notification_show(notification, nullptr);
 
     g_object_unref(G_OBJECT(notification));

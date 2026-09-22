ee8db4ab5dfcc5a9298b0a1871786f35f534d79e
hunterd129 <hunterd129@icloud.com>
2026-09-21 22:33:13 -0600

    Update install.sh
    Updated to cp service & timer to respective location, then reload &
    enable user daemons. binary will also be cp'd to $HOME/.local/bin

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/bash
+set -e
+
+export AUTOMATE_DIR="$HOME/.config/systemd/user"
+export BIN_DIR="$HOME/.local/bin"
 
 mkdir -p bin
+mkdir -p "$AUTOMATE_DIR"
+mkdir -p "$BIN_DIR"
+
+
 
 g++ -std=c++17 -O3 src/main.cpp -o bin/upgrNotif $(pkg-config --cflags --libs libnotify)
 
-mv resources/upgrNotif.service ~/.config/systemd/user/
-mv resources/upgrNotif.timer ~/.config/systemd/user/
+cp resources/upgrNotif.service "$AUTOMATE_DIR"
+cp resources/upgrNotif.timer "$AUTOMATE_DIR"
+cp bin/upgrNotif "$BIN_DIR"
+
+systemctl --user daemon-reload
+systemctl --user enable --now upgrNotif.timer

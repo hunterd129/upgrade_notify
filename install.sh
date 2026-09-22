469d1beb0d52bb7a508cff5556c86d8c0ce42a2d
hunterd129 <hunterd129@icloud.com>
2026-09-22 13:49:30 -0600

    Update install.sh
    removed redundant `mkdir -p bin` command
    and cleaned up lines.

--- install.sh
+++ install.sh
@@ -4,11 +4,8 @@ set -e
 export AUTOMATE_DIR="$HOME/.config/systemd/user"
 export BIN_DIR="$HOME/.local/bin"
 
-mkdir -p bin
 mkdir -p "$AUTOMATE_DIR"
 mkdir -p "$BIN_DIR"
-
-
 
 g++ -std=c++17 -O3 src/main.cpp -o bin/upgrNotif $(pkg-config --cflags --libs libnotify)
 
